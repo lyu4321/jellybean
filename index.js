58f462fec123989f276daab69f510a14c9027698
Leyang Yu <september437@gmail.com>
2021-09-17 14:08:18 -0400

    Code cleanup, removed global variables

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-#!/usr/bin/env node
 import fs, { mkdirSync } from 'fs';
 import path from 'path';
 import yargs from 'yargs';
@@ -7,18 +6,11 @@ import { createRequire } from 'module';
 const require = createRequire(import.meta.url);
 const pkg = require('./package.json');
 
-let currentDirectory = 'dist';
-let currentTemplate = 'layout.html'
-let currentStylesheet = 'style.css';
-let filesArray = [];
-let title = '';
-
 /**
- * Checks to see if a directory exists and if it does, remove it, then create the directory
+ * Checks to see if an output directory exists and if it does, remove it, then create the directory
  * @param {string} directory => path to the directory
  */
 const createDirectory = (directory) => {
-    currentDirectory = directory;
     if (fs.existsSync(directory)) {
         fs.rmdirSync(directory, { recursive: true });
     }
@@ -26,41 +18,33 @@ const createDirectory = (directory) => {
 }
 
 /**
- * Parses a path to get the filename
- * @param {string} path => the path to be parsed
- * @return {string} => returns the filename
- */
-const getFilename = (string) => {
-    return path.parse(path.basename(string)).name;
-}
-
-/**
  * Creates a layout, replacing placeholders with user input
  * @param {string} layout => layout string
- * @param {string} nav => replaces placeholder in layout
- * @param {string} title => replaces placeholder in layout
- * @param {string} body => replaces placeholder in layout
- * @return {string} => returns the layout
+ * @param {string} stylesheet => replaces {stylesheet} placeholder in layout
+ * @param {string} nav => replaces {nav} placeholder in layout
+ * @param {string} title => replaces {title} placeholder in layout
+ * @param {string} body => replaces {body} placeholder in layout
+ * @return {string} => returns the layout as a string
  */
-const getLayout = (layout, nav, title, body) => {
+const getLayout = (layout, stylesheet, title, nav, body) => {
     return layout
-        .replace(/{stylesheet}/g, currentStylesheet)
-        .replace(/{nav}/g, nav)
+        .replace(/{stylesheet}/g, stylesheet)
         .replace(/{title}/g, title)
+        .replace(/{nav}/g, nav)
         .replace(/{body}/g, body)
 }
 
 /**
- * Creates a nav, replacing placeholder with user input
+ * Creates the nav of a page based on the files in a directory
  * @param {array} files => array of file paths
- * @return {string} => returns a string containing the html for navigation links
+ * @return {string} => returns a string of the html used to create the navigation
  */
 const getNav = (files) => {
     if (!Array.isArray(files)) {
         return '';
     } else {
         let links = files.map(file => {
-            let filename = getFilename(file);
+            let filename = path.parse(path.basename(file)).name;
             let name = filename[0].toUpperCase() + filename.slice(1).toLowerCase();
             return `<li><a href='./${filename}.html'>${name}</a></li>`;
         })
@@ -70,123 +54,63 @@ const getNav = (files) => {
 }
 
 /**
- * Creates the body of the page, replacing placeholder with user input
- * @param {string} file => the file text to be used in the body
- * @return {string} => returns a string containing the html for the body 
+ * Creates the title and body of a page from the contents of a .txt file
+ * @param {string} file => the file text 
+ * @return {string} => returns an object containing the string for the title and string for the body 
  */
 const getHtml = (file) => {
+    let html = {
+        title: '',
+        body: ''
+    }
     let tempTitle = file.match(/^.+(\r?\n\r?\n\r?\n)/);
     if (tempTitle) {
-        title = tempTitle[0].trim();
+        html.title = tempTitle[0].trim();
     }
-    return file
+    html.body = file
         .split(/\r?\n\r?\n/)
         .map(para => {
-            if (para == title) {
-                return;
+            if (para == html.title) {
+                `<h1>${para.replace(/\r?\n/, ' ')}</h1>\n`
             } else {
                 return `<p>${para.replace(/\r?\n/, ' ')}</p>\n`;
             }
         })
         .join('');
+    return html;
 }
 
 /**
  * Reads a single file and outputs the contents to a .html file in the output directory
- * @param {string} file => the .txt file to be read
+ * @param {string} file => path to file
+ * @param {string} directory => path to output directory
+ * @param {string} stylesheet => path/URL to stylesheet
+ * @param {string} files => array of all .txt files in a directory
  */
-const readFile = (file) => {
+const readFile = (file, directory, stylesheet, files) => {
     fs.readFile(file, 'utf8', (err, f) => {
         if (err) {
             console.log(err);
         }
+        const nav = getNav(files);
         const html = getHtml(f);
-        const nav = getNav(filesArray);
-        const basename = (getFilename(file));
-        let temp = fs.readFileSync(currentTemplate, 'utf8');
-        let updatedTemplate = getLayout(temp, nav, title, html);
-        fs.writeFile(`${currentDirectory}/${basename}.html`, updatedTemplate, 'utf8', (err) => {
+        const filename = path.parse(path.basename(file)).name;
+        let layout = fs.readFileSync('layout.html', 'utf8');
+        let updatedLayout = getLayout(layout, stylesheet, html.title, nav, html.body);
+        fs.writeFile(`${directory}/${filename}.html`, updatedLayout, 'utf8', (err) => {
             if (err) {
                 console.log(err);
             }
         })
-        if (currentStylesheet == 'style.css') {
-            let style = fs.readFileSync(currentStylesheet, 'utf8');
-            fs.writeFile(`${currentDirectory}/${currentStylesheet}`, style, 'utf8', (err) => {
+        if (stylesheet == 'style.css') {
+            let style = fs.readFileSync(stylesheet, 'utf8');
+            fs.writeFile(`${directory}/${stylesheet}`, style, 'utf8', (err) => {
                 if (err) {
                     console.log(err);
                 }
             })
         }
     })
-}
-
-/**
- * Reads a directory to find all .txt files and performs readFile() on each .txt file
- * @param {string} directory => path to the directory
- */
-const readFiles = (directory) => {
-    fs.readdir(directory, (err, files) => {
-        if (err) {
-            console.log(err);
-        }
-        files.forEach(file => {
-            if (fs.existsSync(`${directory}/${file}`) && path.extname(file) == '.txt') {
-                filesArray.push(file);
-                readFile(`${directory}/${file}`);
-            }
-        })
-    });
-}
-
-/**
- * Checks the user input and if file/directory are valid, reads the file(s)
- * @param {string} input => path to a file/directory
- * @return {boolean} => returns true if the input is valid (file/directory exist, file is of type .txt)
- */
-const checkInput = (input) => {
-    if (fs.existsSync(input)) {
-        if (fs.statSync(input).isFile()) {
-            if (path.extname(input) == '.txt') {
-                readFile(input);
-                return true;
-            }
-        } else if (fs.statSync(input).isDirectory()) {
-            readFiles(input);
-            return true;
-        }
-    }
-    return false;
-}
-
-/**
- * Sets the output directory
- * @param {string} output => sets global directory to user input or 'dist' if no user input
- * @return {boolean} => returns true if the custom output directory is valid
- */
-const getCustomOutput = (output) => {
-    if (output) {
-        if (fs.existsSync(output)) {
-            createDirectory(output);
-            return true;
-        }
-        return false;
-    } else {
-        createDirectory('dist');
-        return true;
-    }
-}
-
-/**
- * Sets the stylesheet 
- * @param {string} stylesheet => set global stylesheet to user input or 'style.css' if no user input
- */
-const getCustomStylesheet = (stylesheet) => {
-    if (stylesheet) {
-        currentStylesheet = stylesheet;
-    } else {
-        currentStylesheet = 'style.css'
-    }
 }
 
 /**
@@ -197,18 +121,49 @@ const getCustomStylesheet = (stylesheet) => {
  * @return {boolean} => returns true if the user input is valid (valid input and output directory) 
  */
 const getUserInput = (input, output, stylesheet) => {
-    if (!input || !checkInput(input)) {
-        return false;
+    let filesArray = [];
+
+    // Setting the output directory
+    if (output) {
+        if (!fs.existsSync(output)) {
+            return false;
+        }
+    } else {
+        output = 'dist';
     }
-    if (!getCustomOutput(output)) {
-        return false;
+    // Setting the stylesheet
+    if (!stylesheet) {
+        stylesheet = 'style.css';
     }
-    getCustomStylesheet(stylesheet);
+    // Setting the input
+    if (!input || !fs.existsSync(input)) {
+        return false;
+    } else {
+        if (fs.statSync(input).isFile() && path.extname(input) == '.txt') {
+            createDirectory(output);
+            readFile(input, output, stylesheet);
+        } else if (fs.statSync(input).isDirectory()) {
+            fs.readdir(input, (err, files) => {
+                if (err) {
+                    console.log(err);
+                }
+                createDirectory(output);
+                filesArray = files.filter(f => path.extname(f) == '.txt');
+                filesArray.forEach(file => {
+                    if (fs.existsSync(`${input}/${file}`)) {
+                        readFile(`${input}/${file}`, output, stylesheet, filesArray);
+                    }
+                })
+            });
+        } else {
+            return false;
+        }
+    }
     return true;
 }
 
 /**
- * Gets the user input from the command line and checks the input, printing an error message if the input is invalid
+ * Gets the user input from the command line and checks the input, printing an error message if the input argument is invalid
  */
 const main = () => {
     const argv = yargs(hideBin(process.argv))
@@ -219,23 +174,23 @@ const main = () => {
         .options('input', {
             alias: 'i',
             demandOption: true,
-            describe: 'A .txt file or folder with .txt files to be generated into a static site',
+            describe: 'Path to a .txt file or folder with files',
             type: 'array'
         })
         .option('output', {
             alias: 'o',
-            describe: 'A custom output directory for generated files to be stored',
+            describe: 'Path to an output directory',
             type: 'string'
         })
         .option('stylesheet', {
             alias: 's',
-            describe: 'A custom stylesheet URL',
+            describe: 'Stylesheet URL',
             type: 'string'
         })
-        .argv
+        .argv;
     let check = getUserInput(argv.input.join(' '), argv.output, argv.stylesheet);
     if (!check) {
-        console.log('Invalid option entered. Please see --help for options.');
+        console.log('Invalid argument entered. Please see --help for options.');
     }
 }
 

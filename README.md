b495891bdc2d6cf3243a352496162e46580afdf0
Gustavo Tavares <gmartinez-de-oliveir@myseneca.ca>
2021-09-23 00:25:24 -0300

    Adding Markdown feature and updating README file

--- README.md
+++ README.md
@@ -4,15 +4,16 @@ From one small program, you can create an entire website. Jellybean is a static 
 
 # Main Features
 
-1. A single .txt file or folder containing multiple files can be converted into HTML pages. 
+1. A single .txt file or folder containing multiple files can be converted into HTML pages.
 2. The title of the page, which is the first line of a file if followed by two blank lines, will be automatically generated.
-3. Generated files are stored in the 'dist' folder and style is provided by 'style.css' by default. Custom folders and styles can be specified using optional flags (see below). 
+3. Generated files are stored in the 'dist' folder and style is provided by 'style.css' by default. Custom folders and styles can be specified using optional flags (see below).
 
 # Installation
 
 1. Clone this repository
 2. Download [Node.js](https://nodejs.org/en/)
 3. Run the following commands
+
 ```diff
 cd jellybean
 npm install
@@ -29,10 +30,10 @@ node index.js -i <folder>
 
 # Optional Flags
 
-| Flag  | Description |
-| ------------- | ------------- |
-| `--output/-o <folder>` | Path to an output directory where generated pages will be stored  |
-| `--stylesheet/-s <URL>`  | Stylesheet URL to be used to style the generated pages  |
+| Flag                    | Description                                                      |
+| ----------------------- | ---------------------------------------------------------------- |
+| `--output/-o <folder>`  | Path to an output directory where generated pages will be stored |
+| `--stylesheet/-s <URL>` | Stylesheet URL to be used to style the generated pages           |
 
 # Getting Help
 
@@ -48,7 +49,7 @@ node index.js --version
 node index.js -v
 ```
 
-# Example Using a File
+# Example Using a Text File
 
 ```diff
 node index.js -i Sherlock-Holmes-Selected-Stories/The Adventure of the Six Napoleans.txt -o customoutput -s https://cdnjs.cloudflare.com/ajax/libs/tufte-css/1.8.0/tufte.min.css
@@ -56,6 +57,7 @@ node index.js -i Sherlock-Holmes-Selected-Stories/The Adventure of the Six Napol
 
 Original File:<br/>
 Sherlock-Holmes-Selected-Stories/The Adventure of the Six Napoleans.txt
+
 ```diff
 THE ADVENTURE OF THE SIX NAPOLEONS
 
@@ -73,6 +75,7 @@ his own vast knowledge and experience.
 
 Generated File:<br/>
 customoutput/The Adventure of the Six Napoleans.html
+
 ```
 <!doctype html>
 <html lang="en">
@@ -92,7 +95,7 @@ customoutput/The Adventure of the Six Napoleans.html
         </ul>
     </div>
     <h1>THE ADVENTURE OF THE SIX NAPOLEONS</h1>
-    <p> 
+    <p>
         It was no very unusual thing for Mr. Lestrade, of Scotland Yard,
         to look in upon us of an evening, and his visits were welcome to
         Sherlock Holmes, for they enabled him to keep in touch with all
@@ -116,15 +119,82 @@ node index.js -i Sherlock-Holmes-Selected-Stories
 ```
 
 In the Sherlock-Holmes-Selected-Stories folder, if you have the files:<br/>
-* notatextfile.js
-* Silver Blaze.txt
-* The Adventure of the Six Napoleans.txt<br/>
+
+- notatextfile.js
+- Silver Blaze.txt
+- The Adventure of the Six Napoleans.txt<br/>
 
 In the dist folder, the following files will be generated:<br/>
-* index.html
-* Silver Blaze.html
-* style.css
-* The Adventure of the Six Napoleons.html
+
+- index.html
+- Silver Blaze.html
+- style.css
+- The Adventure of the Six Napoleons.html
+
+# Additional support for md (markdown) files
+
+For markdown files (.md), the program will parse markdown to HTML tags.
+
+| MD File         | HTML File             | Webpage             |
+| --------------- | --------------------- | ------------------- |
+| `# H1 Heading`  | `<h1>H1 Heading</h1>` | <h1>H1 Heading</h1> |
+| `## H2 Heading` | `<h2>H2 Heading</h2>` | <h2>H2 Heading</h2> |
+| `## H3 Heading` | `<h3>H3 Heading</h3>` | <h2>H3Heading</h2>  |
+
+# Example Using a Markdown File
+
+Original File:<br/>
+README.md
+
+```
+# Jellybean
+
+From one small program, you can create an entire website. Jellybean is a static site generator created in Node.js that lets you easily convert your text files into HTML files.
+
+# Main Features
+
+1. A single .txt file or folder containing multiple files can be converted into HTML pages.
+2. The title of the page, which is the first line of a file if followed by two blank lines, will be automatically generated.
+3. Generated files are stored in the 'dist' folder and style is provided by 'style.css' by default. Custom folders and styles can be specified using optional flags (see below).
+
+# Installation
+
+1. Clone this repository
+2. Download [Node.js](https://nodejs.org/en/)
+3. Run the following commands
+
+```
+
+Generated File:<br/>
+README.html
+
+```
+<!doctype html>
+<html lang="en">
+
+<head>
+    <link rel="stylesheet" href="style.css">
+    <meta charset="utf-8">
+    <title></title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<body>
+    <div><ul><li><a href='./index.html'>Home</a></li><li><a href='./README.html'>README</a></li></ul></div>
+    <h1></h1>
+    <h1>Jellybean</h1>
+<p>From one small program, you can create an entire website. Jellybean is a static site generator created in Node.js that lets you easily convert your text files into HTML files.</p>
+<h1>Main Features</h1>
+<p>1. A single .txt file or folder containing multiple files can be converted into HTML pages. 2. The title of the page, which is the first line of a file if followed by two blank lines, will be automatically generated.
+3. Generated files are stored in the 'dist' folder and style is provided by 'style.css' by default. Custom folders and styles can be specified using optional flags (see below).</p>
+<h1>Installation</h1>
+<p>1. Clone this repository 2. Download [Node.js](https://nodejs.org/en/)
+3. Run the following commands</p>
+
+</body>
+
+</html>
+```
 
 # Live Demo
 
@@ -135,4 +205,5 @@ In the dist folder, the following files will be generated:<br/>
 [Leyang Yu](https://github.com/lyu4321)
 
 # License
+
 [MIT](https://choosealicense.com/licenses/mit/)
--- index.js
+++ index.js
@@ -1,29 +1,29 @@
-import fs, { mkdirSync } from 'fs';
-import path from 'path';
-import yargs from 'yargs';
-import { hideBin } from 'yargs/helpers';
-import { createRequire } from 'module';
+import fs, { mkdirSync } from "fs";
+import path from "path";
+import yargs from "yargs";
+import { hideBin } from "yargs/helpers";
+import { createRequire } from "module";
 const require = createRequire(import.meta.url);
-const pkg = require('./package.json');
+const pkg = require("./package.json");
 
 /**
  * Checks to see if an output directory exists and if it does, remove it, then create the directory
  * @param {string} directory => path to the directory
  */
 const createDirectory = (directory) => {
-    if (fs.existsSync(directory)) {
-        fs.rmdirSync(directory, { recursive: true });
-    }
-    mkdirSync(directory);
-}
+  if (fs.existsSync(directory)) {
+    fs.rmdirSync(directory, { recursive: true });
+  }
+  mkdirSync(directory);
+};
 
 /**
  * Reads the layout.html file contents
  * @return {string} => returns the file contents as a string
  */
 const getLayout = () => {
-    return fs.readFileSync('layout.html', 'utf8');
-}
+  return fs.readFileSync("layout.html", "utf8");
+};
 
 /**
  * Creates a layout, replacing placeholders with user input
@@ -35,12 +35,12 @@ const getLayout = () => {
  * @return {string} => returns the layout as a string
  */
 const getUpdatedLayout = (layout, stylesheet, title, nav, body) => {
-    return layout
-        .replace(/{stylesheet}/g, stylesheet)
-        .replace(/{title}/g, title)
-        .replace(/{nav}/g, nav)
-        .replace(/{body}/g, body)
-}
+  return layout
+    .replace(/{stylesheet}/g, stylesheet)
+    .replace(/{title}/g, title)
+    .replace(/{nav}/g, nav)
+    .replace(/{body}/g, body);
+};
 
 /**
  * Creates the nav of a page based on a file or files in a directory
@@ -48,46 +48,82 @@ const getUpdatedLayout = (layout, stylesheet, title, nav, body) => {
  * @return {string} => returns a string of the html used to create the navigation
  */
 const getNav = (files) => {
-    let index = `<li><a href='./index.html'>Home</a></li>`;
-    if (!Array.isArray(files)) {
-        let filename = path.parse(path.basename(files)).name;
-        return `<div><ul>${index}<li><a href='./${filename}.html'>${filename}</a></li></ul></div>`;
-    } else {
-        let links = files.map(file => {
-            let filename = path.parse(path.basename(file)).name;
-            return `<li><a href='./${filename}.html'>${filename}</a></li>`;
-        })
-            .join(' ');
-        return `<div><ul>${index}${links}</ul></div>`;
-    }
-}
+  let index = `<li><a href='./index.html'>Home</a></li>`;
+  if (!Array.isArray(files)) {
+    let filename = path.parse(path.basename(files)).name;
+    return `<div><ul>${index}<li><a href='./${filename}.html'>${filename}</a></li></ul></div>`;
+  } else {
+    let links = files
+      .map((file) => {
+        let filename = path.parse(path.basename(file)).name;
+        return `<li><a href='./${filename}.html'>${filename}</a></li>`;
+      })
+      .join(" ");
+    return `<div><ul>${index}${links}</ul></div>`;
+  }
+};
 
 /**
  * Creates the title and body of a page from the contents of a .txt file
- * @param {string} file => the file text 
- * @return {string} => returns an object containing the string for the title and string for the body 
+ * @param {string} file => the file text
+ * @return {string} => returns an object containing the string for the title and string for the body
  */
 const getHtml = (file) => {
-    let html = {
-        title: '',
-        body: ''
-    }
-    let tempTitle = file.match(/^.+(\r?\n\r?\n\r?\n)/);
-    if (tempTitle) {
-        html.title = tempTitle[0].trim();
-    }
-    html.body = file
-        .split(/\r?\n\r?\n/)
-        .map(para => {
-            if (para == html.title) {
-                `<h1>${para.replace(/\r?\n/, ' ')}</h1>\n`
-            } else {
-                return `<p>${para.replace(/\r?\n/, ' ')}</p>\n`;
-            }
-        })
-        .join('');
-    return html;
-}
+  let html = {
+    title: "",
+    body: "",
+  };
+  let tempTitle = file.match(/^.+(\r?\n\r?\n\r?\n)/);
+  if (tempTitle) {
+    html.title = tempTitle[0].trim();
+  }
+  html.body = file
+    .split(/\r?\n\r?\n/)
+    .map((para) => {
+      if (para == html.title) {
+        `<h1>${para.replace(/\r?\n/, " ")}</h1>\n`;
+      } else {
+        return `<p>${para.replace(/\r?\n/, " ")}</p>\n`;
+      }
+    })
+    .join("");
+  return html;
+};
+
+/**
+ * Creates the title and body of a page from the contents of a .md file
+ * @param {string} file => the file text
+ * @return {string} => returns an object containing the string for the title and string for the body
+ */
+const getMdToHtml = (file) => {
+  let html = {
+    title: "",
+    body: "",
+  };
+  let tempTitle = file.match(/^.+(\r?\n\r?\n\r?\n)/);
+  if (tempTitle) {
+    html.title = tempTitle[0].trim();
+  }
+  html.body = file
+    .split(/\r?\n\r?\n/)
+    .map((para) => {
+      //work here
+      if (para == html.title) {
+        `<h1>${para.replace(/\r?\n/, " ")}</h1>\n`;
+      } else {
+        let string = para
+          .replace(/^\s*#{1} (.*$)/, "<h1>$1</h1>")
+          .replace(/^\s*#{2} (.*$)/, "<h2>$1</h2>")
+          .replace(/^\s*#{3} (.*$)/, "<h3>$1</h3>");
+
+        return string.startsWith("<h")
+          ? string + "\n"
+          : `<p>${string.replace(/\r?\n/, " ")}</p>\n`;
+      }
+    })
+    .join("");
+  return html;
+};
 
 /**
  * Reads a single file and outputs the contents to a .html file in the output directory
@@ -97,30 +133,41 @@ const getHtml = (file) => {
  * @param {string} files => array of all .txt files in a directory
  */
 const readFile = (file, directory, stylesheet, files) => {
-    fs.readFile(file, 'utf8', (err, f) => {
+  fs.readFile(file, "utf8", (err, f) => {
+    if (err) {
+      console.log(err);
+    }
+    const nav = getNav(files || file);
+    const html = path.extname(file) == ".txt" ? getHtml(f) : getMdToHtml(f);
+    const filename = path.parse(path.basename(file)).name;
+    let layout = getLayout();
+    let updatedLayout = getUpdatedLayout(
+      layout,
+      stylesheet,
+      html.title,
+      nav,
+      html.body
+    );
+    fs.writeFile(
+      `${directory}/${filename}.html`,
+      updatedLayout,
+      "utf8",
+      (err) => {
         if (err) {
-            console.log(err);
+          console.log(err);
         }
-        const nav = getNav(files || file);
-        const html = getHtml(f);
-        const filename = path.parse(path.basename(file)).name;
-        let layout = getLayout();
-        let updatedLayout = getUpdatedLayout(layout, stylesheet, html.title, nav, html.body);
-        fs.writeFile(`${directory}/${filename}.html`, updatedLayout, 'utf8', (err) => {
-            if (err) {
-                console.log(err);
-            }
-        })
-        if (stylesheet == 'style.css') {
-            let style = fs.readFileSync(stylesheet, 'utf8');
-            fs.writeFile(`${directory}/${stylesheet}`, style, 'utf8', (err) => {
-                if (err) {
-                    console.log(err);
-                }
-            })
+      }
+    );
+    if (stylesheet == "style.css") {
+      let style = fs.readFileSync(stylesheet, "utf8");
+      fs.writeFile(`${directory}/${stylesheet}`, style, "utf8", (err) => {
+        if (err) {
+          console.log(err);
         }
-    })
-}
+      });
+    }
+  });
+};
 
 /**
  * Creates an index.html page for the generated site
@@ -129,97 +176,102 @@ const readFile = (file, directory, stylesheet, files) => {
  * @param {array} files => array of all .txt files in a directory
  */
 const writeIndexPage = (output, stylesheet, files) => {
-    let layout = getLayout();
-    let nav = getNav(files);
-    let updatedLayout = getUpdatedLayout(layout, stylesheet, 'Home', nav, '');
-    fs.writeFile(`${output}/index.html`, updatedLayout, 'utf8', (err) => {
-        if (err) {
-            console.log(err);
-        }
-    })
-}
+  let layout = getLayout();
+  let nav = getNav(files);
+  let updatedLayout = getUpdatedLayout(layout, stylesheet, "Home", nav, "");
+  fs.writeFile(`${output}/index.html`, updatedLayout, "utf8", (err) => {
+    if (err) {
+      console.log(err);
+    }
+  });
+};
 
 /**
- * Checks the user input 
+ * Checks the user input
  * @param {string} input => path to a file or directory
  * @param {string} output => path to an output directory
  * @param {string} stylesheet => stylesheet url
- * @return {boolean} => returns true if the user input is valid (valid input and output directory) 
+ * @return {boolean} => returns true if the user input is valid (valid input and output directory)
  */
 const getUserInput = (input, output, stylesheet) => {
-    let filesArray = [];
+  let filesArray = [];
 
-    // Setting the output directory
-    if (output) {
-        if (!fs.existsSync(output)) {
-            return false;
+  // Setting the output directory
+  if (output) {
+    if (!fs.existsSync(output)) {
+      return false;
+    }
+  } else {
+    output = "dist";
+  }
+  // Setting the stylesheet
+  if (!stylesheet) {
+    stylesheet = "style.css";
+  }
+  // Setting the input
+  if (!input || !fs.existsSync(input)) {
+    return false;
+  } else {
+    if (
+      fs.statSync(input).isFile() &&
+      (path.extname(input) == ".txt" || path.extname(input) == ".md")
+    ) {
+      createDirectory(output);
+      writeIndexPage(output, stylesheet, input);
+      readFile(input, output, stylesheet);
+    } else if (fs.statSync(input).isDirectory()) {
+      fs.readdir(input, (err, files) => {
+        if (err) {
+          console.log(err);
         }
+        createDirectory(output);
+        writeIndexPage(output, stylesheet, filesArray);
+        filesArray = files.filter(
+          (f) => path.extname(f) == ".txt" || path.extname(f) == ".md"
+        );
+        filesArray.forEach((file) => {
+          if (fs.existsSync(`${input}/${file}`)) {
+            readFile(`${input}/${file}`, output, stylesheet, filesArray);
+          }
+        });
+      });
     } else {
-        output = 'dist';
+      return false;
     }
-    // Setting the stylesheet
-    if (!stylesheet) {
-        stylesheet = 'style.css';
-    }
-    // Setting the input
-    if (!input || !fs.existsSync(input)) {
-        return false;
-    } else {
-        if (fs.statSync(input).isFile() && path.extname(input) == '.txt') {
-            createDirectory(output);
-            writeIndexPage(output, stylesheet, input);
-            readFile(input, output, stylesheet);
-        } else if (fs.statSync(input).isDirectory()) {
-            fs.readdir(input, (err, files) => {
-                if (err) {
-                    console.log(err);
-                }
-                createDirectory(output);
-                writeIndexPage(output, stylesheet, filesArray);
-                filesArray = files.filter(f => path.extname(f) == '.txt');
-                filesArray.forEach(file => {
-                    if (fs.existsSync(`${input}/${file}`)) {
-                        readFile(`${input}/${file}`, output, stylesheet, filesArray);
-                    }
-                })
-            });
-        } else {
-            return false;
-        }
-    }
-    return true;
-}
+  }
+  return true;
+};
 
 /**
  * Gets the user input from the command line and checks the input, printing an error message if the input argument is invalid
  */
 const main = () => {
-    const argv = yargs(hideBin(process.argv))
-        .help('h')
-        .alias('h', 'help')
-        .version(pkg.name + ' ' + pkg.version)
-        .alias('v', 'version')
-        .options('input', {
-            alias: 'i',
-            demandOption: true,
-            describe: 'Path to a .txt file or folder with files',
-            type: 'array'
-        })
-        .option('output', {
-            alias: 'o',
-            describe: 'Path to an output directory',
-            type: 'string'
-        })
-        .option('stylesheet', {
-            alias: 's',
-            describe: 'Stylesheet URL',
-            type: 'string'
-        })
-        .argv;
-    let check = getUserInput(argv.input.join(' '), argv.output, argv.stylesheet);
-    if (!check) {
-        console.log('Invalid argument entered. Please see --help for options.');
-    }
-}
+  const argv = yargs(hideBin(process.argv))
+    .help("h")
+    .alias("h", "help")
+    .version(pkg.name + " " + pkg.version)
+    .alias("v", "version")
+    .options("input", {
+      alias: "i",
+      demandOption: true,
+      describe: "Path to a .txt file or folder with files",
+      type: "array",
+    })
+    .option("output", {
+      alias: "o",
+      describe: "Path to an output directory",
+      type: "string",
+    })
+    .option("stylesheet", {
+      alias: "s",
+      describe: "Stylesheet URL",
+      type: "string",
+    }).argv;
+
+  let check = getUserInput(argv.input.join(" "), argv.output, argv.stylesheet);
+  if (!check) {
+    console.log("Invalid argument entered. Please see --help for options.");
+  }
+};
 
 main();
